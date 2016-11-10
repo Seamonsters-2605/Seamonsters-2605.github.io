4bbe0fe40f33dac10764ff302b5579031980d0d2
Jacob van't Hoog <vantjac@gmail.com>
2016-11-10 11:42:29 -0800

    Start graph

--- docs/mecanum/wheel-graph.js
+++ docs/mecanum/wheel-graph.js
@@ -1,8 +1,19 @@
 var wheelGraphSketch = function( p ) {
+
+  var graphImage;
   
   p.setup = function() {
     var canvas = p.createCanvas(640, 400);
     canvas.parent('wheel-graph-sketch');
+    
+    graphImage = p.createGraphics(640, 400);
+    for(var x = 0.0; x < graphImage.width; x++) {
+      var xValue = x / graphImage.width * 2*p.PI;
+      var yValue = p.sin(xValue - p.PI/4);
+      
+      var y = (graphImage.height / 2) - (yValue * graphImage.height / 2);
+      p.point(x, y);
+    }
   };
   
   p.mouseClicked = function() {
@@ -14,6 +25,7 @@ var wheelGraphSketch = function( p ) {
   };
   
   p.draw = function() {
+    p.image(graphImage, 0, 0);
     p.text("A graph will be here, eventually.", p.width/2, 8);
   };
   
