cc12b1cca76cb2663d06ead955a56cf7992f6eba
Jacob van't Hoog <vantjac@gmail.com>
2016-11-10 16:00:33 -0800

    Add second function, colors

--- docs/mecanum/wheel-graph.js
+++ docs/mecanum/wheel-graph.js
@@ -7,13 +7,30 @@ var wheelGraphSketch = function( p ) {
     canvas.parent('wheel-graph-sketch');
     
     graphImage = p.createGraphics(640, 400);
+    
+    p.strokeWeight(6);
+    
     for(var x = 0.0; x < graphImage.width; x++) {
       var xValue = x / graphImage.width * 2*p.PI;
-      var yValue = p.sin(xValue - p.PI/4);
+      var yValue1 = p.sin(xValue - p.PI/4);
+      var yValue2 = p.sin(xValue + p.PI/4);
       
-      var y = (graphImage.height / 2) - (yValue * graphImage.height / 2);
-      p.point(x, y);
+      var y1 = (graphImage.height / 2) - (yValue1 * graphImage.height / 3);
+      var y2 = (graphImage.height / 2) - (yValue2 * graphImage.height / 3);
+      
+      if(x != 0) {
+          p.stroke(255, 0, 0);
+          p.line(x-1, prevY1, x, y1);
+          p.stroke(0, 0, 255);
+          p.line(x-1, prevY2, x, y2);
+      }
+      
+      prevY1 = y1;
+      prevY2 = y2;
     }
+    
+    p.strokeWeight(1);
+    p.stroke(0);
   };
   
   p.mouseClicked = function() {
@@ -26,6 +43,7 @@ var wheelGraphSketch = function( p ) {
   
   p.draw = function() {
     p.image(graphImage, 0, 0);
+    p.noStroke();
     p.text("A graph will be here, eventually.", p.width/2, 8);
   };
   
